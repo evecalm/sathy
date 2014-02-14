0701f3865e3566177223b8379151e00826835f87
evcalm.mi <evecalm@live.com>
2014-02-14 18:08:59 +0800

    add semicolon

--- sathy.js
+++ sathy.js
@@ -2,13 +2,14 @@
 ;(function  (window,undefined) {
 	function sathy (selector) {
 		if (!(this instanceof sathy)) {
-			return new sathy(selector)
+			return new sathy(selector);
 		}
 		var ele;
-		if (!(selector instanceof sathy)) {
-			ele = document.querySelectorAll(selector);
-			console.log('instanceof...');
+		if ( selector instanceof sathy ) {
+			return selector;
 		}
+
+		ele = document.querySelectorAll(selector);
 		// console.log('here');
 		this.makeArray( ele );
 		return this;
@@ -16,63 +17,63 @@
 
 
 	sathy.prototype.extend = sathy.extend = function  (config,defaults) {
-		if(!arguments.length) return this
-		var obj = {},key
+		if(!arguments.length) return this;
+		var obj = {},key;
 		if ( 1 === arguments.length) {
 			//if only one argument, extend sathy itself
-			obj = this
+			obj = this;
 		}
 		if(config){
 			for (key in config) {
-				obj[key] = config[key]
+				obj[key] = config[key];
 			}
 			if (defaults) {
 				for(key in defaults){
-					obj[key] = (undefined === obj[key]) ? defaults[key] : obj[key]
+					obj[key] = (undefined === obj[key]) ? defaults[key] : obj[key];
 				}
 			}
 		}
-		return obj
-	}
+		return obj;
+	};
 
 	sathy.extend({
 
 		isArrayLike: function  (obj) {
-			var len
+			var len;
 			//for null,undefined,false,0,''
-			if (!obj) return false
+			if (!obj) return false;
 			//obj is an Array
-			if(obj instanceof Array) return true
-			len = obj.length
+			if(obj instanceof Array) return true;
+			len = obj.length;
 			//length does not exsit in obj or length not a number
-			if (len === undefined || typeof len !== 'number') return false
+			if (len === undefined || typeof len !== 'number') return false;
 			//if length > 0 and obj[length-1] exsit
-			if(len > 0 && obj[len-1] !== undefined) return true
+			if(len > 0 && obj[len-1] !== undefined) return true;
 			//for anything else
-			return false
+			return false;
 		},
 
 		isFunction: function  (fn) {
-			return (fn instanceof Function)
+			return (fn instanceof Function);
 		},
 
 		isLiteral: function(literal){
-			return !(literal == null) && !(literal instanceof Object)
+			return (literal != null) && !(literal instanceof Object);
 		},
 
 		serialize: function  (obj) {
-			var str = [],key
+			var str = [],key;
 			if (obj instanceof Object) {
 				for (key in obj) {
 					//bug: browser origined post action encodes '!' to '%21',
 					//but js' encodeURI and encodeURIComponent don't
 					//even though most server are compatible with this,
 					//the data transported is really different.
-					str.push(encodeURIComponent(key) + '=' + encodeURIComponent(obj[key]))
+					str.push(encodeURIComponent(key) + '=' + encodeURIComponent(obj[key]));
 				}
-				return str.join('&').replace(/!/g, '%21')
+				return str.join('&').replace(/!/g, '%21');
 			} else {
-				return (obj + '')
+				return (obj + '');
 			}
 		},
 
@@ -81,21 +82,21 @@
 			function getXHR () {
 				if (window.XMLHttpRequest) {
 					getXHR = function  () {
-						return new XMLHttpRequest()
-					}
+						return new XMLHttpRequest();
+					};
 				} else if(window.ActiveXObject) {
 					getXHR = function  () {
-						return new ActiveXObject("Microsoft.XMLHTTP")
-					}
+						return new ActiveXObject("Microsoft.XMLHTTP");
+					};
 				} else {
 					getXHR = function  () {
-						throw new Error('XMLHttpRequest is not supported!')
-					}
+						throw new Error('XMLHttpRequest is not supported!');
+					};
 				}
-				return getXHR()
+				return getXHR();
 			}
 			var xhr,key,timer,
-				defaultConfig
+				defaultConfig;
 			defaultConfig = {
 				url:'',
 				method: 'get',
@@ -106,55 +107,55 @@
 				timeout:0,
 				header:null,
 				async:true
-			}
-			xhr = getXHR()
-			config = sathy.extend(config,defaultConfig)
-			xhr.open(config.method,config.url,config.async)
+			};
+			xhr = getXHR();
+			config = sathy.extend(config,defaultConfig);
+			xhr.open(config.method,config.url,config.async);
 			if (config.header instanceof Object) {
 				for (key in header) {
-					xhr.setRequestHeader(key,config.header[key])
+					xhr.setRequestHeader(key,config.header[key]);
 				}
 			}
 			if (config.method.toLowerCase() === 'post') {
-				xhr.setRequestHeader("Content-type","application/x-www-form-urlencoded")
+				xhr.setRequestHeader("Content-type","application/x-www-form-urlencoded");
 			}
 			xhr.onreadystatechange = function  () {
 				if (xhr.readyState == 1) {
 					if (config.async && config.timeout > 0) {
 						setTimeout(function() {
-								xhr.abort('timeout')
-							}, config.timeout)
+								xhr.abort('timeout');
+							}, config.timeout);
 					}
-					return
+					return;
 				}
 				if (xhr.readyState == 4) {
-					clearTimeout(timer)
+					clearTimeout(timer);
 					if (xhr.status >= 200 && xhr.status < 300 || xhr.status === 304) {
 						if (config.success instanceof Function) {
-							var response = xhr.responseText
+							var response = xhr.responseText;
 							if (config.dataType === 'json') {
-								response = sathy.parseJSON(response)
+								response = sathy.parseJSON(response);
 							}
-							config.success.call(null,response)
+							config.success.call(null,response);
 						}
 					} else {
-						xhr.abort('error')
+						xhr.abort('error');
 					}
 				}
-			}
+			};
 			if (config.error instanceof Function) {
-				xhr.onabort = config.error
+				xhr.onabort = config.error;
 			}
-			xhr.send(sathy.serialize(config.data))
-			return xhr
+			xhr.send(sathy.serialize(config.data));
+			return xhr;
 		},
 		//string to JSON,copied from jQuery
 		parseJSON: (window.JSON && JSON.parse) || function( data ) {
 			if ( typeof data !== "string" || !data ) {
-				return null
+				return null;
 			}
 			// Make sure leading/trailing whitespace is removed (IE can't handle it)
-			data = data.replace(/^\s+|\s+$/g,'')
+			data = data.replace(/^\s+|\s+$/g,'');
 
 			// Make sure the incoming data is actual JSON
 			// Logic borrowed from http://json.org/json2.js
@@ -163,44 +164,42 @@
 				.replace(/(?:^|:|,)(?:\s*\[)+/g, "")) ) {
 
 				// Try to use the native JSON parser first
-				return window.JSON && window.JSON.parse ?
-					window.JSON.parse( data ) :
-					(new Function("return " + data))()
+				return (new Function("return " + data))();
 
 			} else {
-				throw new Error('Invalid JSON string')
+				throw new Error('Invalid JSON string');
 			}
 		},
 		//obj to JSON string, copied form JSON.js
 		stringify: (window.JSON && window.JSON.stringify) || function (obj) {
-			var t = typeof (obj)
+			var t = typeof obj;
 			if (t != "object" || obj === null) {
 				// simple data type
-				if (t == "string") obj = '"'+obj+'"'
-				return String(obj)
+				if (t == "string") obj = '"'+obj+'"';
+				return String(obj);
 			}
 			else {
 				// recurse array or object
-				var n, v, json = [], arr = (obj && obj.constructor == Array)
+				var n, v, json = [], arr = (obj && obj.constructor == Array);
 				for (n in obj) {
-					v = obj[n],t = typeof(v)
-					if (t == "string") v = '"'+v+'"'
-					else if (t == "object" && v !== null) v = JSON.stringify(v)
-					json.push((arr ? "" : '"' + n + '":') + String(v))
+					v = obj[n],t = typeof(v);
+					if (t == "string") v = '"'+v+'"';
+					else if (t == "object" && v !== null) v = JSON.stringify(v);
+					json.push((arr ? "" : '"' + n + '":') + String(v));
 				}
-				return (arr ? "[" : "{") + String(json) + (arr ? "]" : "}")
+				return (arr ? "[" : "{") + String(json) + (arr ? "]" : "}");
 			}
 		},
 		each: function  (arr,fn) {
-			var i,len
+			var i,len;
 			if (sathy.isArrayLike(arr) && sathy.isFunction(fn)) {
 				for (i = 0, len = arr.length; i < len; ++i) {
 					// console.log(arr[i]);
-					fn.call(null,arr[i])
+					fn.call(null,arr[i]);
 				}
 			}
 		}
-	})
+	});
 
 	sathy.prototype.extend({
 
@@ -211,148 +210,147 @@
 		splice: Array.prototype.splice,
 
 		makeArray: function (ele) {
-			if (!ele) return
+			if (!ele) return;
 			if (!sathy.isArrayLike(ele)) {
-				ele = [ ele ]
+				ele = [ ele ];
 			}
 			console.log('arr %o',ele);
 			console.log('makeArray %o',Array.prototype.slice.call(ele));
-			Array.prototype.push.apply(this,Array.prototype.slice.call(ele))
-			// Array.prototype.push.apply(this, ele )
+			Array.prototype.push.apply(this,Array.prototype.slice.call(ele));
 		},
 
 		css: function  (name,value) {
-			if (!this.element) return
-			var styleArr,key
+			if (!this.element) return;
+			var styleArr,key;
 			if ((typeof name === 'string') && (typeof value === 'string')) {
-				key = name
-				name = {}
-				name[key] = value
+				key = name;
+				name = {};
+				name[key] = value;
 			}
 			if (name instanceof Object) {
-				styleArr = []
+				styleArr = [];
 				for (key in name) {
-					styleArr.push(key + ':' + name[key])
+					styleArr.push(key + ':' + name[key]);
 				}
 				if (document.all) {
-					this.element.style.cssText += '' + styleArr.join('') + ''
+					this.element.style.cssText += '' + styleArr.join('') + '';
 				} else{
-					this.element.style.cssText += styleArr.join('') + ''
+					this.element.style.cssText += styleArr.join('') + '';
 				}
 			}
-			return this
+			return this;
 		},
 
 		html: function(html){
 			if(!this.element){
 				if (undefined === html) {
-					return this
+					return this;
 				} else {
-					return ''
+					return '';
 				}
 			}
 			if (undefined === html) {
-				return this.element.innerHTML
+				return this.element.innerHTML;
 			} else {
-				this.element.innerHTML = html + ''
-				return this
+				this.element.innerHTML = html + '';
+				return this;
 			}
 		},
 
 		text: function  (text) {
 			if(!this.element){
 				if (undefined === text) {
-					return this
+					return this;
 				} else {
-					return ''
+					return '';
 				}
 			}
 			if (undefined === text) {
-				return this.element.innerText
+				return this.element.innerText;
 			} else {
-				this.element.innerText = text + ''
-				return this
+				this.element.innerText = text + '';
+				return this;
 			}
 		},
 
 		attr: function  (name,value) {
-			var ele = this.element
-			if(undefined == name || !ele) return this
-			name = name + ''
-			if (undefined == value) {
-				return ele.getAttribute(name)
+			var ele = this.element;
+			if(undefined === name || !ele) return this;
+			name = name + '';
+			if (undefined === value) {
+				return ele.getAttribute(name);
 			} else {
 				if (sathy.isLiteral(value)) {
-					ele.setAttribute(name,value)
+					ele.setAttribute(name,value);
 				}
-				return this
+				return this;
 			}
 		},
 
 		removeAttr: function  (name) {
-			var ele = this.element
-			if(!ele || undefined == name) return this
-			ele.removeAttribute(name)
-			return this
+			var ele = this.element;
+			if(!ele || undefined === name) return this;
+			ele.removeAttribute(name);
+			return this;
 		},
 
 		data: function  (name,value) {
 			var ele = this.element;
-			if(!ele) return this
+			if(!ele) return this;
 
 			if (undefined === name) {
 				//to be continue...
 			}
 
 			if (undefined === value) {
-				return ele.getAttribute('data-' + name)
+				return ele.getAttribute('data-' + name);
 			} else {
 				if (sathy.isLiteral(value)) {
-					ele.setAttribute('data-' + name, value)
+					ele.setAttribute('data-' + name, value);
 				}
-				return this
+				return this;
 			}
 		},
 
 		map: function (fn) {
-			sathy.each(this,fn)
+			sathy.each(this,fn);
 		}
-	})
+	});
 
 	if(window.addEventListener) {
 		sathy.prototype.on = function (type,handle) {
 			this.map(function (ele) {
-				ele.addEventListener((type + '').toLowerCase(),handle,false)
-			})
-		}
+				ele.addEventListener((type + '').toLowerCase(),handle,false);
+			});
+		};
 		sathy.prototype.off = function (type,handle) {
 			this.map(function (ele) {
-				ele.removeEventListener((type + '').toLowerCase(),handle,false)
-			})
-		}
+				ele.removeEventListener((type + '').toLowerCase(),handle,false);
+			});
+		};
 	} else if(window.attachEvent) {
 		sathy.prototype.on = function (type,handle) {
 			this.map(function (ele) {
-				ele.attachEvent(('on' + type + '').toLowerCase(),handle)
-			})
-		}
+				ele.attachEvent(('on' + type + '').toLowerCase(),handle);
+			});
+		};
 		sathy.prototype.off = function (type,handle) {
 			this.map(function (ele) {
-				ele.detachEvent(('on' + type + '').toLowerCase(),handle)
-			})
-		}
+				ele.detachEvent(('on' + type + '').toLowerCase(),handle);
+			});
+		};
 	} else {
 		sathy.prototype.on = function (type,handle) {
 			this.map(function (ele) {
-				ele[('on' + type + '').toLowerCase()] = handle
-			})
-		}
+				ele[('on' + type + '').toLowerCase()] = handle;
+			});
+		};
 		sathy.prototype.off = function (type) {
 			this.map(function (ele) {
-				ele[('on' + type + '').toLowerCase()] = null
-			})
-		}
+				ele[('on' + type + '').toLowerCase()] = null;
+			});
+		};
 	}
 
-	window.$ = window.sathy= sathy
-})(window)
+	window.$ = window.sathy= sathy;
+})(window);
